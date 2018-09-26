e114990e4b783493634d96e3e34d4997ba435c4d
Gayatri295 <42969247+Gayatri295@users.noreply.github.com>
2018-09-26 11:04:04 +0530

    Laptop

---
+++ Laptop/src/Laptop.java
@@ -0,0 +1,12 @@
+
+public class Laptop {
+	public double price;
+    public String model;
+    public double width;
+    public String company;
+    public int ratings;
+    public int warrentyPeriod;
+    public int id;
+    public double height;
+
+}
